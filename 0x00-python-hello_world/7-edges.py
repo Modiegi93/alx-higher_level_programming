c9557e0963b67c923aabb19c08951f8748b6d9e8
Mathapelo <modiegi.mphahlele93@gmail.com>
2022-11-28 21:22:30 -0800

    Task 6 - 9

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first = [:3]
+word_last_2 = [-2:]
+middle_word = [1:-1]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print("Middle word: {}".format(middle_word))
