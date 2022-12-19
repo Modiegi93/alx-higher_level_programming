3a4eda259876a59e32b97e2483e0a84f177ed7f6
Mathapelo <modiegi.mphahlele93@gmail.com>
2022-12-19 07:13:21 -0800

    fixed task 10

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,4 +1,7 @@
-#include <python3.8/Python.h>
+#include <Python.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
