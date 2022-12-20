9dab39548830d303411c4920b0e323890ceab89c
Mathapelo <modiegi.mphahlele93@gmail.com>
2022-12-20 09:19:54 -0800

    Task 0 - 10

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" creates a class Square"""
+
+class Square:
+    """ class for a Square"""
+
+    def __init__(self,size):
+        """ Initialize class """
+        self.__size = size
