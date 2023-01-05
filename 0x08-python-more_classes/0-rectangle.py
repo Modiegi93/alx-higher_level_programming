192017ab31906a77fa55cd3d26b99a1739c238b8
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-01-05 05:03:49 -0800

    All tasks

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+This is a module for a class Rectangle
+"""
+
+class Rectangle:
+    """Class of a rectangle"""
+
+    def __init__(self):
+        """Initialize class"""
+        pass
