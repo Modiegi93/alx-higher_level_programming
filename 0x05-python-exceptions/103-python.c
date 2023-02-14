35b7bdd2d256d3d07d4b3a391f78d4f46cf8d30c
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-02-14 10:30:46 -0800

    updates

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,7 +1,10 @@
-#include <Python.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <object.h>
+#include <listobject.h>
+#include <bytesobject.h>
+#include <floatobject.h>
 
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
