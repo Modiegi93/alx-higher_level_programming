5c20deb28a41eb4b1f579581498b1713a4fa9623
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-03-13 03:24:29 -0700

    initial commit

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Write a script that prints 3 lines
+const myFun = 'C is fun';
+const myCool = 'Python is cool';
+const myAmazing = 'JavaScript is amazing';
+
+console.log(myFun);
+console.log(myCool);
+console.log(myAmazing);
