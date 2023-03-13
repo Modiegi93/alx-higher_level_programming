11e9da8516140c1248edf4e0f280684c9855a881
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-03-13 08:25:36 -0700

    fixing errors

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,13 +6,11 @@ const size = parseInt(process.argv[2]);
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  let square = '';
-
   for (let i = 0; i < size; i++) {
+    let square = '';
     for (let j = 0; j < size; j++) {
       square += 'X';
     }
-    square += '\n';
-  }
-  console.log(square);
+    console.log(square);
+}
 }
