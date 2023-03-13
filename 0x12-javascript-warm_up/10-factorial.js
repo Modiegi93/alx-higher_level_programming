1bc08dd23cd02f80a83e90f519a6d7f61c7944f9
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-03-13 09:23:51 -0700

    fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,19 +1,15 @@
 #!/usr/bin/node
 // computes and prints a factorial
 
-function computeFactorial (num) {
-  if (isNaN(num)) {
+const factorial = (n) => {
+  if (isNaN(n)) {
     return 1;
-  } else if (num === 0) {
+  } else if (n === 0 || n === 1) {
     return 1;
   } else {
-    return num * computeFactorial(num - 1);
+    return n * factorial(n - 1);
   }
-}
+};
 
-const arg = process.argv[2];
-const input = parseInt(arg);
-
-const factorial = computeFactorial(input);
-
-console.log(`${input} is ${factorial}`);
+const n = parseInt(process.argv[2]);
+console.log(factorial(n));
