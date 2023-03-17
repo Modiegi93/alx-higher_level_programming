2010e8531f4dbdf0c75af18d7fa3a2d7708cb730
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-03-17 01:20:42 -0700

    task7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,29 +3,25 @@
 """
 lists all State objects from the database hbtn_0e_6_usa
 """
-
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 if __name__ == '__main__':
+    engine = create_engine("mysql://{}:{}@localhost:3306/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+        
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
 
-    if len(sys.argv) != 4:
-        print("""Usage: {} <mysql username> <mysql password>
-              <database name>""".format(sys.argv[0]))
-        sys.exit(1)
+    session = Session()
 
-        engine = create_engine("mysql://{}:{}@localhost:3306/{}".format(
-                               sys.argv[1], sys.argv[2], sys.argv[3]))
-        Base.metadata.create_all(engine)
+    states = session.query(State).order_by(State.id).all()
 
-        Session = sessionmaker(bind=engine)
-        session = Session()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
 
-        states = session.query(State).order_by(State.id).all()
-        for state in states:
-            print("{}: {}".format(state.id, state.name))
-
-        session.close()
-        engine.dispose
+    session.close()
