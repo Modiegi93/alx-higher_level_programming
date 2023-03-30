9bfb8e6285dffab760ae47000ea5ab6fed2223cb
Mathapelo Mphahlele <modiegi.mphahlele93@gmail.com>
2023-03-30 04:32:14 -0700

    fixed

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # request to 0.0.0.0:5000/catch_me that causes the server to respond
-curl -s -o /dev/null -w "You got me!" -X PUT "http://0.0.0.0:5000/catch_me"
+curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
