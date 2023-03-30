d4f34229c8d05104450166d5f6c187f5f52d1229
Mathapelo Mphahlele <modiegi.mphahlele93@gmail.com>
2023-03-30 05:26:00 -0700

    fix rest

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a POST request to the passed URL,
-# and displays the body of the response
-curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" $1
+# takes in a URL, sends a POST request to the passed URL,and displays the body of the resp
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" -L
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,15 +2,30 @@
 
 
 def find_peak(list_of_integers):
-    """Find a peak in a list of unsorted integers."""
-    if not list_of_integers:
+    """ finds a peak in a list of unsorted integers."""
+
+    if list_of_integers is None or len(list_of_integers) == 0:
         return None
-    left = 0
-    right = len(list_of_integers) - 1
-    while left < right:
-        mid = (left + right) // 2
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            right = mid
+
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+
+    mid_idx = int(len(list_of_integers) / 2)
+
+    if mid_idx != len(list_of_integers) - 1:
+        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx] and\
+           list_of_integers[mid_idx + 1] < list_of_integers[mid_idx]:
+            return list_of_integers[mid_idx]
+    else:
+        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx]:
+            return list_of_integers[mid_idx]
         else:
-            left = mid + 1
-    return list_of_integers[left]
+            return list_of_integers[mid_idx - 1]
+
+    if list_of_integers[mid_idx - 1] > list_of_integers[mid_idx]:
+        a_list = list_of_integers[0:mid_idx]
+    else:
+        a_list = list_of_integers[mid_idx + 1:]
+
+    return find_peak(a_list)
+
