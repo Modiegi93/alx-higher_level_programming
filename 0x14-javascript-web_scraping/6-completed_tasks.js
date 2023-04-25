4cbc6f0ed96c56288d788a877afa75b08bada3f5
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-04-25 13:03:16 +0000

    commit

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,24 +4,24 @@ const request = require('request');
 
 const url = process.argv[2];
 
-request.get(url, (err, res, body) => {
+request.get(url, function (err, _res, body) {
   if (err) {
-    console.error(err);
-    return;
-  }
-  const todos = JSON.parse(body);
-  const completedTasksByUser = {};
-  for (const todo of todos) {
-    if (todo.completed) {
-      const userId = todo.userId;
-      if (completedTasksByUser[userId]) {
-        completedTasksByUser[userId]++;
-      } else {
-        completedTasksByUser[userId] = 1;
+    console.log(err);
+  } else {
+    const completedTasksByUsers = {};
+    body = JSON.parse(body);
+
+    for (let i = 0; i < body.length; ++i) {
+      const userId = body[i].userId;
+      const completed = body[i].completed;
+
+      if (completed && !completedTasksByUsers[userId]) {
+        completedTasksByUsers[userId] = 0;
       }
+
+      if (completed) ++completedTasksByUsers[userId];
     }
-  }
-  for (const userId in completedTasksByUser) {
-    console.log(`'${userId}' : ${completedTasksByUser[userId]},`);
+
+    console.log(completedTasksByUsers);
   }
 });
