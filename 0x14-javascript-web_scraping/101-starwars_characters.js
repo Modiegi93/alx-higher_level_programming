264c10f9ff5d93fcd33a20d316803171d394dd06
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-04-25 13:32:59 +0000

    fix

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,19 +4,16 @@ const request = require('request');
 
 const movieId = process.argv[2];
 
-// Make a request to the Star Wars API for the specified movie
-request(`https://swapi.dev/api/films/${movieId}/`, (error, response, body) => {
+request(`https://swapi.dev/api/films/${movieId}/`, function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
     const data = JSON.parse(body);
 
-    // Get the list of characters for the movie
     const characters = data.characters;
 
-    // Print each character's name
     characters.forEach((characterUrl) => {
-      request(characterUrl, (error, response, body) => {
+      request(characterUrl, function (error, response, body) {
         if (error) {
           console.error(error);
         } else {
